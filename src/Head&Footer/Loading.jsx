aaf6f57b0dcb660aeb4bdab79e9b650d7a772fac
GerVaf <thantzinmin667@gmail.com>
2023-11-23 14:18:06 +0630

    v0

--- src/Head&Footer/Loading.jsx
+++ src/Head&Footer/Loading.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import './Loading.css'
 const Loading = () => {
   return (
-    <div class="loading-screen flex gap-5">
+    <div class="loading-screen flex gap-5 items-center justify-center">
       <div class="loading">
         <span></span>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <p>Loading please wait...</p>
+      <p className="sm:text-base text-[10px]">Loading please wait...</p>
     </div>
   );
 };
