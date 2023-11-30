5ee4df801a1dd76cc09b195f966f1389d0ccbd01
GerVaf <thantzinmin667@gmail.com>
2023-11-30 09:34:14 +0630

    v0

--- src/assets/ProjectData.js
+++ src/assets/ProjectData.js
@@ -3,9 +3,65 @@ import image2 from "./img/TeamPortfolio.png";
 import image4 from "./img/shop.png";
 import image5 from "./img/gameplatform.png";
 import image6 from "./img/companyPortfolio.png";
+import image7 from "./img/artist_portfolio.png";
+import image8 from "./img/Opaque_portfolio.png";
 
 import image3 from "./img/shopping.png";
 export const ProjectsDisplay = [
+  {
+    id: 7,
+    title: "Opaque Portfolio",
+    img: image8,
+    link: "https://com-portfolio-oe8b.vercel.app/",
+    language: [
+      {
+        title: "react",
+      },
+      {
+        title: "tailwind",
+      },
+      {
+        title: "framer-motion",
+      },
+      {
+        title: "lenis",
+      },
+      {
+        title: "react-icons",
+      },
+      {
+        title: "react-router-dom",
+      },
+    ],
+  },
+  ,
+  {
+    id: 7,
+    title: "Artist Portfolio",
+    img: image7,
+    link: "https://opaque-six.vercel.app/",
+    language: [
+      {
+        title: "react",
+      },
+      {
+        title: "tailwind",
+      },
+      {
+        title: "framer-motion",
+      },
+      {
+        title: "react-dom",
+      },
+      {
+        title: "react-icons",
+      },
+      {
+        title: "react-router-dom",
+      },
+    ],
+  },
+  ,
   {
     id: 6,
     title: "Company Portfolio",
@@ -13,21 +69,28 @@ export const ProjectsDisplay = [
     link: "https://company-portfolio-pied.vercel.app/",
     language: [
       {
-        title: 'react'
-      },{
-        title: 'tailwind'
-      },{
-        title: 'framer-motion'
-      },{
-        title: 'react-dom'
-      },{
-        title: 'react-icons'
-      },{
-        title: 'react-router-dom'
-      },{
-        title: 'react-typical'
-      },{
-        title: 'typewriter-effect'
+        title: "react",
+      },
+      {
+        title: "tailwind",
+      },
+      {
+        title: "framer-motion",
+      },
+      {
+        title: "react-dom",
+      },
+      {
+        title: "react-icons",
+      },
+      {
+        title: "react-router-dom",
+      },
+      {
+        title: "react-typical",
+      },
+      {
+        title: "typewriter-effect",
       },
     ],
   },
@@ -38,25 +101,34 @@ export const ProjectsDisplay = [
     link: "https://game-platform-tawny.vercel.app/",
     language: [
       {
-        title: 'react'
-      },{
-        title: 'tailwind'
-      },{
-        title: 'react-router-dom'
-      },{
-        title: 'react-fast-marquee'
-      },{
-        title: 'react-icons'
-      },{
-        title: 'react-lazyload'
-      },{
-        title: 'react-redux'
-      },{
-        title: 'redux-persist'
-      },{
-        title: 'react-toastify'
-      },{
-        title: 'swiper'
+        title: "react",
+      },
+      {
+        title: "tailwind",
+      },
+      {
+        title: "react-router-dom",
+      },
+      {
+        title: "react-fast-marquee",
+      },
+      {
+        title: "react-icons",
+      },
+      {
+        title: "react-lazyload",
+      },
+      {
+        title: "react-redux",
+      },
+      {
+        title: "redux-persist",
+      },
+      {
+        title: "react-toastify",
+      },
+      {
+        title: "swiper",
       },
     ],
   },
@@ -67,27 +139,37 @@ export const ProjectsDisplay = [
     link: "https://order-project-8s69qv41b-gervaf.vercel.app/",
     language: [
       {
-        title: 'react'
-      },{
-        title: 'tailwind'
-      },{
-        title: 'mantine-carousel'
-      },{
-        title: 'axios'
-      },{
-        title: 'daisyui'
-      },{
-        title: 'firebase'
-      },{
-        title: 'fontawesome-icon'
-      },{
-        title: 'framer-motion'
-      },{
-        title: 'react-dom'
-      },{
-        title: 'react-fast-marquee'
-      },{
-        title: 'react-hot-toast'
+        title: "react",
+      },
+      {
+        title: "tailwind",
+      },
+      {
+        title: "mantine-carousel",
+      },
+      {
+        title: "axios",
+      },
+      {
+        title: "daisyui",
+      },
+      {
+        title: "firebase",
+      },
+      {
+        title: "fontawesome-icon",
+      },
+      {
+        title: "framer-motion",
+      },
+      {
+        title: "react-dom",
+      },
+      {
+        title: "react-fast-marquee",
+      },
+      {
+        title: "react-hot-toast",
       },
     ],
   },
@@ -98,17 +180,22 @@ export const ProjectsDisplay = [
     link: "https://auth-seven-dun.vercel.app/",
     language: [
       {
-        title: 'react'
-      },{
-        title: 'react-icons'
-      },{
-        title: 'react-owl-carousel'
-      },{
-        title: 'swiper'
-      },{
-        title: 'react-compare-image'
-      },{
-        title: 'web-vitals'
+        title: "react",
+      },
+      {
+        title: "react-icons",
+      },
+      {
+        title: "react-owl-carousel",
+      },
+      {
+        title: "swiper",
+      },
+      {
+        title: "react-compare-image",
+      },
+      {
+        title: "web-vitals",
       },
     ],
   },
@@ -119,17 +206,22 @@ export const ProjectsDisplay = [
     link: "https://shopping-site-alpha.vercel.app/",
     language: [
       {
-        title: 'react'
-      },{
-        title: 'react-dom'
-      },{
-        title: 'react-icons'
-      },{
-        title: 'react-slick'
-      },{
-        title: 'slick-carousel'
-      },{
-        title: 'swiper'
+        title: "react",
+      },
+      {
+        title: "react-dom",
+      },
+      {
+        title: "react-icons",
+      },
+      {
+        title: "react-slick",
+      },
+      {
+        title: "slick-carousel",
+      },
+      {
+        title: "swiper",
       },
     ],
   },
@@ -140,17 +232,22 @@ export const ProjectsDisplay = [
     link: "https://team-portfolio-seven.vercel.app/",
     language: [
       {
-        title: 'react'
-      },{
-        title: 'react-dom'
-      },{
-        title: 'react-icons'
-      },{
-        title: 'flowbite'
-      },{
-        title: 'react-typical'
-      },{
-        title: 'swiper'
+        title: "react",
+      },
+      {
+        title: "react-dom",
+      },
+      {
+        title: "react-icons",
+      },
+      {
+        title: "flowbite",
+      },
+      {
+        title: "react-typical",
+      },
+      {
+        title: "swiper",
       },
     ],
   },
